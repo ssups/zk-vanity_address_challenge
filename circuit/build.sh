b410635f185a21b65ea8259c76278817dbaa1b6a
ssups <shalstlqruki@gmail.com>
2025-07-05 02:37:11 +0900

    feat: add nulifier

--- circuit/build.sh
+++ circuit/build.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-rm -rf ./target/*
+rm -rf ./target/vanity_address_challenge.json
+rm -rf ./target/vk
 
 echo "Compiling circuit..."
 if ! nargo compile; then
@@ -10,23 +11,13 @@ if ! nargo compile; then
     exit 1
 fi
 
-# nargo execute witness.gz &&
-# echo "Witness generated successfully." &&
-# bb prove --scheme ultra_honk --oracle_hash keccak -b ./target/vanity_address_challenge.json -w ./target/witness.gz -o ./target &&
-# echo "Proof generated successfully." &&
+cp ./target/vanity_address_challenge.json ../client/src/zk/circuit.json
 
 echo "Generating vkey..."
-
 bb write_vk -b ./target/vanity_address_challenge.json -o ./target --oracle_hash keccak
-# bb write_vk --scheme ultra_honk -b ./target/vanity_address_challenge.json -o ./target
-# bb write_vk --scheme ultra_honk --oracle_hash keccak -b ./target/vanity_address_challenge.json -o ./target
-
-# bb verify --scheme ultra_honk --oracle_hash keccak -k ./target/vk -p ./target/proof
 
 
 echo "Generating solidity verifier..."
-
 bb write_solidity_verifier -k ./target/vk -o ../contract/src/Verifier.sol
-# bb write_solidity_verifier --scheme ultra_honk -k ./target/vk -o ../contract/src/Verifier.sol
 
 echo "Done"
--- circuit/run.sh
+++
@@ -1,9 +0,0 @@
-# !/bin/bash
-
-nargo execute witness.gz &&
-echo "Witness generated successfully." &&
-bb prove --scheme ultra_honk --oracle_hash keccak -b ./target/vanity_address_challenge.json -w ./target/witness.gz -o ./target &&
-echo "Proof generated successfully." &&
-bb write_vk --scheme ultra_honk --oracle_hash keccak -b ./target/vanity_address_challenge.json -o ./target &&
-echo "Verification key generated successfully." &&
-bb verify --scheme ultra_honk --oracle_hash keccak -k ./target/vk -p ./target/proof
